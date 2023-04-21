91aa9ba12e31d368249964870caa09bb2388eaf3
Иван Соколов <vano95.cokol@gmail.com>
2023-04-21 14:09:25 +0600

    done

--- UI.py
+++ UI.py
@@ -5,29 +5,29 @@ from commands import  get_notice, find_notice, find_notice_date, print_date,\
 del_notice,change_notice,add_notice
 def menu(num):
   if numb == 1:
-    get_notice()
-  elif numb == 2:
-    id = input('Введите ID заметки: ')
-    find_notice(id)
-  elif numb == 3:
     topic = input('Введите заголовок заметки: ')
     text = input('Введите текст заметки: ')
     date = print_date()
     note = Main_Note(str(counter()),topic,text,date)
     add_notice(note)
+  elif numb == 2:
+    get_notice()
+  elif numb == 3:
+    id = input('Введите ID заметки: ')
+    find_notice(id)
   elif numb == 4:
-    id = input('Введите ID контакта для удаления: ')
-    del_notice(id)
+    date = input('Введите дату для поиска заметки: ')
+    find_notice_date(date)
   elif numb == 5:
       id = input('Введите ID контакта для изменения: ')
       change_notice(id)
   elif numb == 6:
-    date = input('Введите дату для поиска заметки: ')
-    find_notice_date(date)
+    id = input('Введите ID контакта для удаления: ')
+    del_notice(id)
 while True:
-  numb = int(input('Введите 1 - для печати всех заметок; 2 - для поиска \
-заметки по ID;''3 - для добавления заметки; 4 - для удаления заметки; 5 - для \
-изменения заметки; 6 - для поиска заметки по дате; 7 - для выхода из программы: '))
+  numb = int(input('Введите 1 - для добавления заметки; 2 - для печати всех заметок;'' 3 - для поиска \
+заметки по ID;''4 - для поиска заметки по дате;''5 - для \
+изменения заметки;'' 6 - для удаления заметки;''7 - для выхода из программы: '))
   if numb == 7:
     break 
   menu(numb)
--- commands.py
+++ commands.py
@@ -2,45 +2,76 @@
 from counter import counter
 import datetime
 import os
+from Note import Main_Note
 def get_notice(): #вывод всех заметок
-  with open ('Notice.txt', 'r', encoding='utf8') as file:
+  with open ('Notice.json', 'r', encoding='utf8') as file:
     for line in file:
       print (line)
 
 def find_notice(id): #поиск заметки по id и вывод ее
-  with open('Notice.txt', 'r') as file:
+  with open('Notice.json', 'r') as file:
       for line in file:
           if line.startswith(f'{id};'):
               print(line)
 
 
 def add_notice (note): # добавление заметки
-  with open ('Notice.txt', 'a', encoding='utf8') as file:
+  with open ('Notice.json', 'a', encoding='utf8') as file:
     file.write(note.str_1())
 
 
-def find_notice_date(date): #поиск заметки по date и вывод ее
+def find_notice_date(date): #поиск заметки по дате и ее вывод 
+    with open('Notice.json', 'r', encoding='utf-8') as file:
+        found = False
+        for line in file:
+            data = line.split(';')
+            note_date = datetime.datetime.strptime(data[3].strip(), '%d.%m.%Y') 
+            if note_date.date() == datetime.datetime.strptime(date, '%d.%m.%Y').date():
+                found = True 
+                print(line.strip()) 
+        if not found: 
+            print('Заметка с этой датой не найдена.')
 
-
-
-def del_notice(id):
-    with open('Notice.txt', 'r') as file:
-        with open('temp.txt', 'w') as temp:
+def del_notice(id): #удаление заметки
+    with open('Notice.json', 'r') as file:
+        with open('temp.json', 'w') as temp:
             for line in file:
                 if line.startswith(f'{id};'):
                     continue
                 temp.write(line)
     file.close()
     temp.close()
-    os.replace('temp.txt', 'Notice.txt')
+    os.replace('temp.json', 'Notice.json')
 
 
 
-def change_notice(id):
+def change_notice(id):#изменение заметки
+    notes = []
+    with open('Notice.json', 'r', encoding='utf8') as file:
+        for line in file:
+            data = line.strip().split(";")
+            if len(data) == 4:
+                note = Main_Note(data[0], data[1], data[2], data[3])
+                notes.append(note)
+            else:
+                print("Ошибка: строка имеет неправильный формат.")
 
-    file.write(input ('Введите новый текст заметки: ')) 
+    for note in notes:
+        if note.id == id:
+            topic = input('Введите новый заголовок заметки: ')
+            text = input('Введите новый текст заметки: ')
+            date = print_date()
+            note.topic = topic
+            note.text = text
+            note.date = date
+            with open('Notice.json', 'w', encoding='utf8') as file:
+                for note in notes:
+                    file.write(f'{note.id};{note.topic};{note.text};{note.date}\n')
+            break
+    else:
+        print('Заметка с таким ID не найдена.')
 
-def print_date():
+def print_date(): #добавление даты в заметку
     now = datetime.datetime.now()
     timestamp = now.strftime("%d.%m.%Y")
     return timestamp
